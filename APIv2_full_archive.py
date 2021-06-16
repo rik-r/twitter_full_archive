a9f7300f4fc76df70dd6ab5d83cf932b8bbe3844
Rik <55724286+rik-r@users.noreply.github.com>
2021-06-16 03:16:30 -0500

    Add files via upload

--- APIv2_full_archive.py
+++ APIv2_full_archive.py
@@ -31,7 +31,6 @@ def search_endpoint_connect(bearer_token, query, st, et, next_token):
 
 def main(bearer_token, n, fn, sq, st, et):
     
-    rl_count = 0
     count = 0
     flag = True
     first = True
@@ -39,9 +38,6 @@ def main(bearer_token, n, fn, sq, st, et):
 
     while flag:
         
-        if rl_count==300:
-            time.sleep(600)
-            print('Rate limit cooldown 10 mins.')
 
         if count >= n and n!=0:
             break
@@ -54,23 +50,22 @@ def main(bearer_token, n, fn, sq, st, et):
         if 'next_token' in json_response['meta']:
             next_token = json_response['meta']['next_token']
         
-            if result_count is not None and result_count > 0 and next_token is not None:
+        if result_count is not None and result_count > 0:
             
-                df = pd.json_normalize(json_response['data'])
-                df = df.reindex(columns=['id', 'author_id', 'created_at', 'text', 'lang', 'public_metrics.retweet_count',
+            df = pd.json_normalize(json_response['data'])
+            df = df.reindex(columns=['id', 'author_id', 'created_at', 'text', 'lang', 'public_metrics.retweet_count',
                                      'public_metrics.reply_count', 'public_metrics.like_count', 'public_metrics.quote_count',
                                     'in_reply_to_user_id', 'geo.place_id', 'geo.coordinates.type', 'geo.coordinates.coordinates'])
-                if not first:
-                    df.to_csv('%s.csv'%fn, mode='a', encoding='utf-8', index=False, header=None)
-                else:
-                    df.to_csv('%s.csv'%fn, encoding='utf-8', index=False)
+            if not first:
+                df.to_csv('%s.csv'%fn, mode='a', encoding='utf-8', index=False, header=None)
+            else:
+                df.to_csv('%s.csv'%fn, encoding='utf-8', index=False)
 
-                time.sleep(1)
-                count += result_count
-                print('Tweets downloaded: '+str(count))
-        else:
+        time.sleep(3.1)
+        count += result_count
+        print('Tweets downloaded: '+str(count))
+        if 'next_token' not in json_response['meta']:
             flag = False
-        rl_count += 1
         first = False
 
 
@@ -84,12 +79,12 @@ no_of_tweets = 20
 file_name = 'downloaded_tweets'
 
 #Enter your search query. Refer to https://developer.twitter.com/en/docs/twitter-api/tweets/search/integrate/build-a-query
-search_query = '(nostalgia OR #nostalgia) lang:en -is:retweet -is:reply place_country:US'
+search_query = '#your #query #here'
 
 #Set the beginning date and time in YYYY-MM-DDTHH:MM:SSZ format
-start_time = "2019-11-27T00:00:00Z"
+start_time = "YYYY-MM-DDTHH:MM:SSZ"
 
 #Set the ending date and time in YYYY-MM-DDTHH:MM:SSZ format
-end_time = "2019-11-28T23:59:59Z"
+end_time = "YYYY-MM-DDTHH:MM:SSZ"
 
 main(bearer_token, no_of_tweets, file_name, search_query, start_time, end_time)
